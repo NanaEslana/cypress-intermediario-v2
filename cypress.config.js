4d6e83b1ea2e5a4a1531b7edbf455d7252864362
Eslana Regina <reginaeslana2011@gmail.com>
2024-12-20 16:58:48 -0300

    feat:creating functions and commands

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost',
+    baseUrl: 'https://intranet.ctism.ufsm.br',
   },
   fixturesFolder: false,
   video: false,
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -3,11 +3,24 @@ Cypress.Commands.add('login', (
     password = Cypress.env('user_password'),
 ) => {
     const login = () => {
-        cy.visit('/users/sign_in')
-
+        cy.visit('/cadastrousuario/logintest.html')
         cy.get("[data-qa-selector='login_field']").type(user)
         cy.get("[data-qa-selector='password_field']").type(password, { log: false })
         cy.get("[data-qa-selector='sign_in_button']").click()
     }
     login()
+});
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
+});
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
 })
